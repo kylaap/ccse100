210cbecfa7f760e53f3404a7c7d456ce99dad225
kylaap <kapowel8@asu.edu>
2025-01-24 14:07:22 -0500

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,40 @@
+/* 
+Kyla Powell
+CSE 100
+homework 1
+*/
+
+/*
+The problem solving process, as described in section 1.6 of the textbook, follows 11 simple steps. 
+1. Define what the prgram is to do.
+2. Visualize the program running on the computer
+3. Use design tools to create a model of the program.
+4. Check the model for logical errors. 
+5. Write the program source code. 
+6. Compile the source code
+7. Correct any errors found during the compliation
+8. Link the program to create an executable file
+9. Run the program using test data for input
+10. Correct any errors found while running the program. Repeat steps 4 through 10 as many times as necessary
+11. Validate the results of the program
+Step 1 requires you to simply identify the purpose of the program. Step 2 helps you visuals what you are doing. 
+Step 3 is to help you format and create the program. Step 4 helps you find and eliminate errors. Step 5 is for writinng the source code. 
+Step 6 has you compile the source code. meaning it will translate the code into machine language, as a compiler will do.
+Step 7 is similar to step 4, you will find errors and eliminate them. Step 8 is used to create an exutable file. 
+Step 9 and 10 are both steps that check for errors again. Step 11, the last step, is simply to solve the problem. */
+
+/* 
+// Names.cpp
+//Prints a list of student names with their hometowns
+//and intended major
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Name: Kyla Powell" << endl;
+    cout << "Hometown: Knoxville, TN" << endl;
+    cout << "Major: Chemistry" << endl;
+    return 0;
+}
+// The program is to print out the name, hometown, and major of the student.
